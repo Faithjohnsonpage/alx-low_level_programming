1e4001887f94fd2a07b21ed66dfef9ca1e35faf2
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-07-18 14:23:24 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void);
+{
+	char c = 'a'
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+}

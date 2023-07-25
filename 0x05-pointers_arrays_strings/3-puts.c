0392463bc741782b4a48f4a5df2979c971758669
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-07-25 08:51:39 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: a string variable
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	str = "abc123";
+
+	_putchar(str);
+	_putchar('\n');
+}

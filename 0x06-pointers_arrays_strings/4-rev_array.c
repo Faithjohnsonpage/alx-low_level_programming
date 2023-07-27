74c3292c255f7cbfbf1fbf953c47d0088582b420
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-07-27 10:05:03 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int ch;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; (i < n / 2); i++)
 	{
 		ch = a[i];
-		a[i] = a[n - i];
-		a[n - i] = ch;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = ch;
 	}
 }

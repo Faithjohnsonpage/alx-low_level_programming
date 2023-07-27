76399a10e031077128c4ac71f9bf2edac9b7884d
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-07-27 11:44:17 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @ch: input value
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *ch)
+{
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if (ch[i] >= 97 && ch[i] <= 122)
+		{
+			ch[i] - 32;
+		}
+	}
+	return (ch);
+}

2622b95ba6bc4c12baaaf82de146a8c8c4d0d7aa
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-07-31 13:44:56 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches for a set of bytes
+ * @s: str1
+ * @accept: str2
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		sum++;
+	}
+	return ('\0');
+}

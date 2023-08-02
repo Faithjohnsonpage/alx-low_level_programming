4904e33d8a98605c3bfd5f4a02d3acf188635653
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-02 12:07:53 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_recursion -  function that prints a string
+ * _puts_recursion - functions like puts();
  * @s: string
  * Return: nothing
  */
@@ -10,7 +10,7 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else

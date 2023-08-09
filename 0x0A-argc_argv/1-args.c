61c1316fa27dc9a0f212a1dc969e085d704dac14
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-09 05:44:00 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	char *unused_argument __attribute__((unused)); /* Declare an unused variable */
+	(void)argv; /* Declare an unused variable */
 	printf("%d"\n, argc - 1);
 
 	return (0);

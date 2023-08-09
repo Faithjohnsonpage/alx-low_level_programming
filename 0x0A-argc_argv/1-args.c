d9b7be4d41bbd4ed7f00de16f78fffb953cee2cf
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-09 05:39:06 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	char *unused_argument __attribute__((unused)); //Declare an unused variable
+	printf("%d"\n, argc);
+
+	return (0);
+}

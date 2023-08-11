34976d8126e106fda61f9e8e78ef8bd6e7bd6009
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-11 14:57:17 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		pee = malloc(new_size);
-		return (p);
+		return (pee);
 	}
 	else if (new_size == 0)
 	{

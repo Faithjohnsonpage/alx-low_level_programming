2e4c5c2b96ed148c70e210580abadc8741133c6a
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-15 15:20:25 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,11 +11,11 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (*s++)
 	{
 		i++;
 	}
-	return (s);
+	return (i);
 }
 
 /**
@@ -55,23 +55,22 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int l1, l2;
 
-	l1 = _strlen(name);
-	l2 = _strlen(owner);
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (l1 + 1));
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(char) * (l2 + 1));
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
@@ -79,8 +78,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
+	dog->name = _strcpy(dog->name, name);
+	dog->owner = _strcpy(dog->owner, owner);
 	dog->age = age;
 
 	return (dog);

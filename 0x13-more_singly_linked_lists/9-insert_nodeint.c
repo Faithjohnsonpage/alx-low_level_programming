b6d3d7de70187e12dc3751663882a7b977bd4dd8
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-29 04:13:48 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	newNode->n = n;
+	newNode->next = NULL;
+
 	if (idx == 0)
 	{
 		newNode->next = *head;

c9009fa47f2feeffe7afbab8a10de0d3fbaaee3f
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-08-29 05:03:12 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 
-	for (i = 0; current != NULL && i < idx; ++i)
+	for (i = 0; current != NULL && i < idx - 1; ++i)
 	{
 		current = current->next;
 		if (current == NULL)

97209e41ba621d7b47407c04269ae1b22f7e65fb
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-09-04 15:02:45 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,22 +19,35 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
 	{
-		perror("open");
-		return (-1);
-	}
-
-	if (text_content != NULL)
-	{
-		nb_append = write(fd, text_content, strlen(text_content));
-		if (nb_append == -1)
+		if (errno == ENOENT)
 		{
-			perror("write");
-			close(fd);
 			return (-1);
 		}
+		else if (errno == EACCES)
+		{
+			return (-1);
+		}
+		else
+		{
+			perror("open");
+			return (-1);
+		}
+	}
+
+	nb_append = write(fd, text_content, strlen(text_content));
+
+	if (nb_append == -1)
+	{
+		perror("write");
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);

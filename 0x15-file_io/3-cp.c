a99b47627f478220be445c69a30092458c144059
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-09-05 12:13:58 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,89 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * copy_file_func - Copies the content of one file to another.
+ * @file_from: The name of the source file.
+ * @file_to: The name of the destination file.
+ *
+ * Return: Always returns 0 on successful completion.
+ */
+int copy_file_func(const char *file_from, const char *file_to)
+{
+	int fd1, fd2;
+	int nb_read, nb_write;
+	char buffer[1024];
+
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		close(fd1);
+		exit(99);
+	}
+
+	while ((nb_read = read(fd1, buffer, 1024)) > 0)
+	{
+		nb_write = write(fd2, buffer, nb_read);
+		if (nb_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			close(fd1);
+			close(fd2);
+			exit(99);
+		}
+	}
+
+	if (nb_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd1);
+		close(fd2);
+		exit(98);
+	}
+
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+
+	return (0);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: Always returns 0 on successful completion.
+ */
+int main(int argc, char **argv)
+{
+	int res;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	res = copy_file_func(argv[1], argv[2]);
+	dprintf(STDOUT_FILENO, "%i\n", res);
+	return (0);
+}

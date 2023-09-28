85e061f14af1166034dd71e6f0a15902f55d5068
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-09-28 08:31:01 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,8 @@
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointer to a string of 0 and 1 chars.
  *
- * Return: the converted number, or 0 if b is NULL or contains non-binary chars.
+ * Return: the converted number, or 0 if b is NULL 
+ * or contains non-binary chars.
  */
 unsigned int binary_to_uint(const char *b)
 {

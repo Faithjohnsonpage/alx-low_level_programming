401fb79a11e44daae62c6412df6c7c5cb86e94ab
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-10-02 04:18:03 -0700

    3-cp.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,7 @@
-#include <unistd.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -13,33 +11,27 @@
  *
  * Return: 1 on success, -1 on failure.
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, nb_append;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
+	if (text_content != NULL)
 	{
-		if (errno == ENOENT)
-		{
-			return (-1);
-		}
-		else if (errno == EACCES)
-		{
-			return (-1);
-		}
-		else
-		{
-			perror("open");
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
 }

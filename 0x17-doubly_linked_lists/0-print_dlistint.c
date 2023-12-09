d1587035866925064863377a2dc67dd1f20e2ce4
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-09 13:33:17 +0100

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,22 +3,22 @@
 
 /**
  * print_dlistint - A function to print the elements in the nodes
- * @dlistint_t: A head pointer
- * Return: Always EXIT_SUCCESS.
+ * @h: A head pointer
+ * Return: The number of elements in the list.
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	dlistint_t *temp;
+	const dlistint_t *temp;
 
 	temp = h;
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		count++;
-		temp =temp->next;
+		temp = temp->next;
 	}
 
-	return count;
+	return (count);
 }

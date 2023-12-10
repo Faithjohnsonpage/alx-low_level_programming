7fe1987a9876d7bef152dc7abbfcfb359e83c451
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-10 11:27:08 +0100

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,6 +19,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		temp = *head;
 		*head = (*head)->next;
+		if (*head != NULL)
+            (*head)->prev = NULL;
 		free(temp);
 
 		return (1);
@@ -33,8 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	temp->prev->next = temp->next;
-    if (temp->next != NULL)
-        temp->next->prev = temp->prev;
+	temp->next->prev = temp->prev;
 	free(temp);
 
 	return (1);

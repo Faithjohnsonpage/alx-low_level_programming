212f97c343850829f8b763b893fa6445fb80d718
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-22 12:54:35 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,9 +8,27 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
 	table->size = size;
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
 
 	return (table);
 }

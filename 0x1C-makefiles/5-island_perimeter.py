515f0e5f02864e9ab00c73d4af0759273e6f7cea
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-02-23 11:46:02 +0100

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,11 +11,17 @@ def island_perimeter(grid):
 
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 0:
-                    continue
-                elif grid[i][j] == 1 and perimeter == 0:
-                    perimeter += 4
-                elif grid[i][j] == 1 and perimeter > 0:
-                    perimeter += 2
+                if grid[i][j] == 1:
+                    perimeter += 4  # Assume full perimeter
+                    # Check neighbors and subtract 1 for each adjacent land
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 1
+                    if i < rows - 1 and grid[i + 1][j] == 1:
+                        perimeter -= 1
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 1
+                    if j < cols - 1 and grid[i][j + 1] == 1:
+                        perimeter -= 1
+
         return perimeter
     return 0  # Return 0 if grid is empty

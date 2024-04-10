34ae2079eedf143d12495cc671bab4062e22b952
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-10 13:38:51 +0100

    104-advanced_binary.c

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+int recursiveBinarySearch(int *array, size_t start, size_t end, int value);
+
+/**
+ * advanced_binary - Searches for the first occurrence of a value in a
+ * sorted array using binary search algorithm.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If value is not present in the array or array is NULL, returns -1.
+ * Otherwise, returns the index of the first occurrence of value in the array.
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t start = 0, end = size - 1;
+	int index;
+
+	if (array == NULL)
+		return (-1);
+
+	index = recursiveBinarySearch(array, start, end, value);
+	return (index);
+}
+
+/**
+ * recursiveBinarySearch - Recursive helper function for binary search
+ * algorithm.
+ * @array: A pointer to the first element of the array to search.
+ * @start: The index of the start of the current subarray.
+ * @end: The index of the end of the current subarray.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present in the current subarray, returns -1.
+ * Otherwise, returns the index of first occurrence of value in the subarray.
+ */
+
+int recursiveBinarySearch(int *array, size_t start, size_t end, int value)
+{
+	size_t mid, i;
+
+	if (end >= start)
+	{
+		printf("Searching in array: ");
+		for (i = start; i <= end; i++)
+		{
+			if (i == end)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+
+		mid = start + (end - start) / 2;
+		if (array[mid] == value)
+		{
+			if (mid == start || array[mid - 1] != value)
+				return (mid);
+			else
+				return (recursiveBinarySearch(array, start, mid, value));
+		}
+
+		if (array[mid] > value)
+		{
+			return (recursiveBinarySearch(array, start, mid - 1, value));
+		}
+		return (recursiveBinarySearch(array, mid + 1, end, value));
+	}
+
+	return (-1);
+}
